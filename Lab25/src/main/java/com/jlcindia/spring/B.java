71438c40380e4f3305f8c9c399581a856a8288d9
AZAM <AZAM@AZAM-HP>
2016-06-28 23:41:48 +0530

    initialCommit

---
+++ Lab25/src/main/java/com/jlcindia/spring/B.java
@@ -0,0 +1,31 @@
+package com.jlcindia.spring;
+
+public class B {
+	
+	private int b;
+	private String str;
+	/*public B(int b, String str) {
+		System.out.println("B 2arg C");
+		this.b = b;
+		this.str = str;
+	}
+	
+
+
+/*
+	public void setB(int b) {
+		this.b = b;
+	}
+
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+	*/
+	
+	public String toString() {
+		return "" + b+"\t"+ str ;
+	}
+
+
+}
